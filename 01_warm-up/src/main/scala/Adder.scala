dd8c6e21d5dd10dfb0eb31ef16f745ef2353b8ce
Udaya Raj Subedi <075bei047.udaya@pcampus.edu.np>
2024-11-14 22:55:36 +0100

    Adding the design for half adder, full adder and fourbit adder

--- 01_warm-up/src/main/scala/Adder.scala
+++ 01_warm-up/src/main/scala/Adder.scala
@@ -24,12 +24,17 @@ class HalfAdder extends Module{
     /* 
      * TODO: Define IO ports of a half adder as presented in the lecture
      */
+    val a = Input(UInt(1.W))
+    val b = Input(UInt(1.W))
+    val s = Output(UInt(1.W))
+    val cout = Output(UInt(1.W))
     })
 
   /* 
    * TODO: Describe output behaviour based on the input values
    */
-
+    io.s := io.a ^ io.b
+    io.cout := io.a & io.b
 }
 
 /** 
@@ -49,17 +54,30 @@ class FullAdder extends Module{
     /* 
      * TODO: Define IO ports of a half adder as presented in the lecture
      */
+    val a = Input(UInt(1.W))
+    val b = Input(UInt(1.W))
+    val cin = Input(UInt(1.W))
+    val s = Output(UInt(1.W))
+    val cout = Output(UInt(1.W))
+
     })
 
 
   /* 
    * TODO: Instanciate the two half adders you want to use based on your HalfAdder class
    */
-
+  val halfAdder1 = Module(new HalfAdder())
+  val halfAdder2 = Module(new HalfAdder())
 
   /* 
    * TODO: Describe output behaviour based on the input values and the internal signals
    */
+  halfAdder1.io.a := io.a
+  halfAdder1.io.b := io.b
+  halfAdder2.io.a := halfAdder1.io.s
+  halfAdder2.io.b := io.cin
+  io.s := halfAdder2.io.s
+  io.cout := halfAdder1.io.cout | halfAdder2.io.cout
 
 }
 
@@ -79,14 +97,48 @@ class FourBitAdder extends Module{
     /* 
      * TODO: Define IO ports of a 4-bit ripple-carry-adder as presented in the lecture
      */
+     val a = Input(UInt(4.W))       // 4-bit input A
+    val b = Input(UInt(4.W))       // 4-bit input B
+    val sum = Output(UInt(4.W))    // 4-bit sum output
+    val cout = Output(UInt(1.W))  // Carry-out output
     })
 
   /* 
    * TODO: Instanciate the full adders and one half adderbased on the previously defined classes
    */
-
+  val halfAdder = Module(new HalfAdder())
+  val fullAdder1 = Module(new FullAdder())
+  val fullAdder2 = Module(new FullAdder())
+  val fullAdder3 = Module(new FullAdder())
 
   /* 
    * TODO: Describe output behaviour based on the input values and the internal 
    */
+  val sumBits = Wire(Vec(4, Bool())) // this is required we can't assign io.sum(0) := halfAdder.io.sum
+  //val carryBits = Wire(Vec(4, Bool()))
+  // Connect the first half adder for the LSB
+  halfAdder.io.a := io.a(0)
+  halfAdder.io.b := io.b(0)
+  sumBits(0) := halfAdder.io.s
+
+  // Connect the carry-out of the half adder to the first full adder
+  fullAdder1.io.a := io.a(1)
+  fullAdder1.io.b := io.b(1)
+  fullAdder1.io.cin := halfAdder.io.cout
+  sumBits(1) := fullAdder1.io.s
+
+  // Connect the remaining full adders in series for each subsequent bit
+  fullAdder2.io.a := io.a(2)
+  fullAdder2.io.b := io.b(2)
+  fullAdder2.io.cin := fullAdder1.io.cout
+  sumBits(2) := fullAdder2.io.s
+
+  fullAdder3.io.a := io.a(3)
+  fullAdder3.io.b := io.b(3)
+  fullAdder3.io.cin := fullAdder2.io.cout
+  sumBits(3) := fullAdder3.io.s
+
+  // The final carry-out is the carry from the last full adder
+  io.cout := fullAdder3.io.cout
+  io.sum := sumBits.asUInt
 }
