796ed1fbae28dd3725db643db807995f53ff89dc
mrigakshipandey <mrigakshipandey@gmail.com>
2025-03-24 14:25:22 +0100

    adds caching logic

--- 06_project_task/src/main/scala/BranchTargetBuffer.scala
+++ 06_project_task/src/main/scala/BranchTargetBuffer.scala
@@ -5,6 +5,8 @@ The goal of this task is to implement a Branch Target Buffer (BTB).
 1. How does a higher associativity affect the performance of a cache?
   - The increased associativity improves cache hit rates but at the cost of more complex logic and possibly higher latency.
 2. What’s the best initial state for the 2-bit predictor FSM? What effects would different initial states have in regular program patterns (e.g., loops)?
+  - Weak Not taken
+    Refernece [https://www.youtube.com/watch?v=AWv8DCm_UYE]
 3. Find a way to implement a structure of registers with 8 sets and 2 ways. Standard memory classes might not be a good option here.
 4. How can the number of index bits be determined?
   - We will use 3 bits to address the 8 sets
@@ -52,6 +54,8 @@ class BranchTargetBuffer () extends Module {
     val predictedTaken = Output(Bool())
   })
 
+  printf("//// Read Cache\n")
+
   // Initialize Outputs
   io.valid  := 0.U
   io.target := 0.U
@@ -91,7 +95,77 @@ class BranchTargetBuffer () extends Module {
 
   // Check for cache hit and set prediction state (taken or not)
   val predictedTaken = Mux(hit_vector(0), set(0).predictor(1), Mux(hit_vector(1), set(1).predictor(1), false.B))
-  
+  printf("predictedTaken  = %d\n",predictedTaken)
+
+  printf("//// Update Cache\n")
+  // BTB Update Logic (Occurs 2 cycles later at the EX stage)
+  when(io.update) {
+    val updateIndex = io.updatePC(4, 2)
+    val updateTag = io.updatePC(31, 5)
+
+    // Determine if there's a matching entry
+    val way0 = cache(updateIndex)(0)
+    val way1 = cache(updateIndex)(1)
+
+    val hit0 = way0.valid && (way0.tag === updateTag)
+    val hit1 = way1.valid && (way1.tag === updateTag)
+
+    val updateWay = Mux(hit0, 0.U, Mux(hit1, 1.U, Mux(lru(updateIndex), 1.U, 0.U)))
+
+    /* In a 2-bit predictor state machine, we have the following states:
+        - 00 (Strong Not Taken)
+            The next states are:
+            - 00, when mispredicted 0
+            - 01, when mispredicted 1
+        - 01 (Weak Not Taken)
+            The next states are:
+            - 00, when mispredicted 0
+            - 10, when mispredicted 1
+        - 10 (Strong Taken)
+            The next states are:
+            - 10, when mispredicted 0
+            - 11, when mispredicted 1            
+        - 11 (Weak Taken)
+            The next states are:
+            - 10, when mispredicted 0
+            - 00, when mispredicted 1            
+        
+        io.mispredicted tells us if our prediction, as defined by MSB was wrong
+
+        We also notice that it acts like a two bit counter when misprediction is always 1
+        Additionaly, for weak convictions we can do a -1 when the prediction was correct
+    */
+
+    // If no existing entry, create a new one
+    when(!hit0 && !hit1) {
+      cache(updateIndex)(updateWay).valid := true.B
+      cache(updateIndex)(updateWay).tag := updateTag
+      cache(updateIndex)(updateWay).target := io.updateTarget
+
+      printf("new prediction set\n")
+
+      // We are starting at Weak Not Taken, so the next state is set accordingly
+      cache(updateIndex)(updateWay).predictor := Mux(io.mispredicted,
+        2.U, // Move towards Strongly Taken
+        0.U // Move towards Strongly Not Taken
+      )
+    }.otherwise {
+      // Update predictor FSM for existing entry
+      val oldState = cache(updateIndex)(updateWay).predictor
+      printf("current prediction  = %d\n",oldState)
+      
+      cache(updateIndex)(updateWay).predictor := Mux(io.mispredicted,
+        oldState + 1.U,                                      // 2-bit counter behaviour
+        Mux(oldState(0) === 1.U, oldState, oldState - 1.U) // -1 for weak convictions
+
+      )
+    }
+        
+    
+    // since this way was just updated the other way is lru
+    // Update LRU bit
+    lru(updateIndex) := !updateWay 
+  }
 
   // Assign values to output
   io.target := target
--- 06_project_task/src/test/scala/BranchTargetBuffer_tb.scala
+++ 06_project_task/src/test/scala/BranchTargetBuffer_tb.scala
@@ -10,14 +10,123 @@ class BranchTargetBufferTest extends AnyFlatSpec with ChiselScalatestTester {
 "BranchTargetBuffer_Tester" should "work" in {
     test(new BranchTargetBuffer()).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
 
+      //////////// TEST Default Case
+
       // Send a PC for the first time, expect a miss
-      dut.io.update.poke(false.B)
       dut.io.PC.poke(0x1000.U)
-      dut.clock.step(1)
-  
+
+      dut.io.update.poke(false.B)
+      
+      dut.clock.step(1)  
       dut.io.valid.expect(false.B)
-      dut.io.target.expect(0x1004.U)
+      dut.io.target.expect(0x1004.U) // Default PC + 4 
       dut.io.predictedTaken.expect(false.B)
+
+      
+      
+      //////////// TEST the 2-bit Prediction FSM
+
+      // Update
+      dut.io.PC.poke(0x100A.U)
+
+      dut.io.update.poke(true.B)
+      dut.io.updatePC.poke(0x1000.U)
+      dut.io.updateTarget.poke(0x1F80.U)
+      dut.io.mispredicted.poke(true.B)
+      // This would make the next state 11
+      
+      dut.clock.step(1)
+      dut.io.valid.expect(false.B)
+      dut.io.target.expect(0x100E.U)
+      dut.io.predictedTaken.expect(false.B)
+
+      // Check Updated
+      dut.io.PC.poke(0x1000.U)
+
+      dut.io.update.poke(false.B)
+            
+      dut.clock.step(1)  
+      dut.io.valid.expect(true.B)
+      dut.io.target.expect(0x1F80.U)
+      dut.io.predictedTaken.expect(true.B)
+
+      // Update
+      dut.io.PC.poke(0x100A.U)
+
+      dut.io.update.poke(true.B)
+      dut.io.updatePC.poke(0x1000.U)
+      dut.io.updateTarget.poke(0x1F80.U)
+      dut.io.mispredicted.poke(true.B)
+      // This would make the next state 00
+      
+      dut.clock.step(1)
+      dut.io.valid.expect(false.B)
+      dut.io.target.expect(0x100E.U)
+      dut.io.predictedTaken.expect(false.B)
+
+      // Check Updated
+      dut.io.PC.poke(0x1000.U)
+
+      dut.io.update.poke(false.B)
+            
+      dut.clock.step(1)  
+      dut.io.valid.expect(true.B)
+      dut.io.target.expect(0x1F80.U)
+      dut.io.predictedTaken.expect(true.B)
+
+      // Update
+      dut.io.PC.poke(0x100A.U)
+
+      dut.io.update.poke(true.B)
+      dut.io.updatePC.poke(0x1000.U)
+      dut.io.updateTarget.poke(0x1F80.U)
+      dut.io.mispredicted.poke(true.B)
+      // This would make the next state 01
+      
+      dut.clock.step(1)
+      dut.io.valid.expect(false.B)
+      dut.io.target.expect(0x100E.U)
+      dut.io.predictedTaken.expect(false.B)
+
+      // Check Updated
+      dut.io.PC.poke(0x1000.U)
+
+      dut.io.update.poke(false.B)
+            
+      dut.clock.step(1)  
+      dut.io.valid.expect(true.B)
+      dut.io.target.expect(0x1F80.U)
+      dut.io.predictedTaken.expect(false.B)
+
+      //////////// Cache Replacement
+
+      // Add a new Entry to the same set
+      dut.io.PC.poke(0x1000.U)
+
+      dut.io.update.poke(true.B)
+      dut.io.updatePC.poke(0x2000.U)
+      dut.io.updateTarget.poke(0x1A80.U)
+      dut.io.mispredicted.poke(true.B)
+      
+      dut.clock.step(1)
+      dut.io.valid.expect(true.B)
+      dut.io.target.expect(0x1F80.U)
+      dut.io.predictedTaken.expect(false.B)
+
+      // Add another new Entry to the same set
+      dut.io.PC.poke(0x1000.U)
+
+      dut.io.update.poke(true.B)
+      dut.io.updatePC.poke(0x3000.U)
+      dut.io.updateTarget.poke(0x1180.U)
+      dut.io.mispredicted.poke(true.B)
+      
+      // Expect a miss
+      dut.clock.step(1)  
+      dut.io.valid.expect(false.B)
+      dut.io.target.expect(0x1004.U) // Default PC + 4 
+      dut.io.predictedTaken.expect(false.B)
+
     }
   }
 }
